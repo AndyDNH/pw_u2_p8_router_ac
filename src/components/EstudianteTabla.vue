06e0eb2e3261360be645b15d3cc66b75cc84ef5c
Andres Cueva <132870444+AndyDNH@users.noreply.github.com>
2025-05-30 19:22:34 -0500

    Taller 20

--- src/components/EstudianteTabla.vue
+++ src/components/EstudianteTabla.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="container">
-
     <div v-show="mostrar">
-      <h1>Estudiante Guardado</h1>
-      
+      <h1>{{ mensajeFinal }}</h1>
     </div>
     <div class="textos">
       <label for="id_nombre">Nombre</label>
       <input v-model="nuevoNombre" id="id_nombre" type="text" />
+      <span v-if="mensaje.nombre">{{ mensaje.nombre }}</span>
 
       <label for="id_apellido">Apellido</label>
       <input v-model="nuevoApellido" id="id_apellido" type="text" />
+      <span v-if="mensaje.apellido">{{ mensaje.apellido }}</span>
 
       <label for="id_genero">Género</label>
       <input v-model="nuevoGenero" id="id_genero" type="text" />
@@ -25,36 +25,35 @@
     <br />
     <button v-on:click="agregarEstudiante"><b>Agregar</b></button>
 
-   
-
     <table border="1">
-            <thead>
-                <!--tr siempre es primero y representa una fila, primero se contruye tanto en la cabecera como el cuerpo-->
-                <tr>
-                    <!--Como contenido de la fila declaro todas las columnas-->
-                    <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>Genero</th>
-                    <th>edad</th>
-                    <th>Correo</th>
-                    <th>Accion</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="{ nombre, apellido, genero, edad, correo } in lista"
-        :key="nombre + apellido">
-                    <!--Para la columna ya no declaro un th si no que un elemento td-->
-                    <td>{{ nombre }}</td>
-                    <td>{{apellido}}</td>
-                    <td>{{ genero }}</td>
-                    <td>{{ edad }}</td>
-                    <td>{{ correo }}</td>
-                    <td><button>Ver</button></td>
-                </tr>
-                <!--Por cada fila es un elemento html tr-->
-                
-            </tbody>
-        </table>
+      <thead>
+        <!--tr siempre es primero y representa una fila, primero se contruye tanto en la cabecera como el cuerpo-->
+        <tr>
+          <!--Como contenido de la fila declaro todas las columnas-->
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Genero</th>
+          <th>edad</th>
+          <th>Correo</th>
+          <th>Accion</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="{ nombre, apellido, genero, edad, correo } in lista"
+          :key="nombre + apellido"
+        >
+          <!--Para la columna ya no declaro un th si no que un elemento td-->
+          <td>{{ nombre }}</td>
+          <td>{{ apellido }}</td>
+          <td>{{ genero }}</td>
+          <td>{{ edad }}</td>
+          <td>{{ correo }}</td>
+          <td><button>Ver</button></td>
+        </tr>
+        <!--Por cada fila es un elemento html tr-->
+      </tbody>
+    </table>
   </div>
 </template>
  
@@ -62,11 +61,11 @@
 export default {
   data() {
     return {
-      nuevoNombre: "",
-      nuevoApellido: "",
-      nuevoGenero: "",
-      nuevaEdad: 20,
-      nuevoCorreo: "",
+      nuevoNombre: null,
+      nuevoApellido: null,
+      nuevoGenero: null,
+      nuevaEdad: null,
+      nuevoCorreo: null,
       lista: [
         {
           nombre: "Andres",
@@ -118,26 +117,70 @@ export default {
           correo: "anahis@gmail.com",
         },
       ],
-      mostrar:true,
+      mostrar: false,
+      nombreMensaje: false,
+      apellidoMensaje: false,
+      mensaje: {
+        nombre: null,
+        apellido: null,
+      },
+      mensajeFinal:null,
     };
   },
   methods: {
     agregarEstudiante() {
-      const nuevo = {
-        nombre: this.nuevoNombre,
-        apellido: this.nuevoApellido,
-        genero: this.nuevoGenero,
-        edad: this.nuevaEdad,
-        correo: this.nuevoCorreo,
-      };
-      // this.lista.unshift(nuevo);
-      this.lista.push(nuevo);
-      this.mostrar = true;
-      this.nombre = null;
+      if (this.validarEntradas() == true) {
+        const nuevo = {
+          nombre: this.nuevoNombre,
+          apellido: this.nuevoApellido,
+          genero: this.nuevoGenero,
+          edad: this.nuevaEdad,
+          correo: this.nuevoCorreo,
+        };
+        // this.lista.unshift(nuevo);
+        this.lista.push(nuevo);
+        this.mostrar = true;
+        this.nombre = null;
 
-      setTimeout(() => {
-        this.mostrar=false;
-      }, 3000);
+        setTimeout(() => {
+          this.mostrar = false;
+        }, 3000);
+        this.mensajeFinal="Estudiante Guardado"
+        this.limpiarPagina();
+      }
+    },
+    validarEntradas() {
+      try {
+
+      
+      // let validar= this.mensaje.apellido.primero;
+      let numero = 2;
+      if (this.nuevoNombre === null) {
+        this.mensaje.nombre = "Nombre es Obligatorio";
+        numero--;
+      };
+      if (this.nuevoApellido === null) {
+        this.mensaje.apellido = "Apellido es Obligatorio";
+        numero--;
+      };
+      if (numero === 2){
+        return true;
+      }else {
+        return false;
+      }
+      }catch(error){
+        console.error("Ha ocurriido un problema");
+        console.error(error);
+        this.mostrar = true;
+        this.mensajeFinal= "Ha ocurrido un error del sistema";
+      }
+    
+  
+    },
+    limpiarPagina(){
+      this.nombre = null;
+      this.mensaje.nombre = null;
+      this.mensaje.apellido = null;
     },
   },
 };
@@ -191,20 +234,19 @@ table {
   width: 100%;
   border-collapse: separate;
   border-spacing: 0;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   font-size: 0.95rem;
   background-color: #fff;
   box-shadow: 0 8px 24px rgba(0, 0, 0, 0.05);
   border-radius: 8px;
   overflow: hidden;
   margin: 20px;
-  
 }
- 
+
 thead {
   background-color: #f4f6f8;
 }
- 
+
 thead th {
   text-align: C;
   padding: 15px;
@@ -212,24 +254,22 @@ thead th {
   font-weight: 600;
   border-bottom: none;
 }
- 
+
 tbody tr {
   transition: background-color 0.2s;
 }
- 
+
 tbody tr:nth-child(even) {
   background-color: #7bacdd;
 }
- 
+
 tbody tr:hover {
   background-color: #9da0a5;
 }
- 
+
 tbody td {
   padding: 15px;
   border-bottom: 1px solid #e3e6ea;
   color: #444;
 }
-
-
 </style>
